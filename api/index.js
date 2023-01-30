fb7b9d6aa044919a05d1dd867c35027b417eddcd
Yuya Hochi <yuya1charo@gmail.com>
2023-01-30 01:13:03 +0000

    complete storing access token in session storage and refresh token in cookie, started to implement auto-login

--- api/index.js
+++ api/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-
+const cookieParser = require("cookie-parser");
+const cookie = require("cookie");
 const app = express();
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  credentials: true
+}));
 
 const users = [
   {
@@ -35,6 +40,24 @@ const generateRefreshToken = (user) => {
   return jwt.sign({ id: user.id, isAdmin: user.isAdmin }, "myRefreshSecretKey");
 };
 
+app.post("/api/auto-login", (req, res) => {
+  const refreshToken = req.cookies.refreshToken;
+  if (!refreshToken) return res.status(401).json("You need to log-in");
+  jwt.verify(refreshToken, "myRefreshSecretKey", (err, user) => {
+    err && console.log(err);
+
+    const newAccessToken = generateAccessToken(user);
+
+    refreshTokens.push(refreshToken);
+
+    res.status(200).json({
+      username: user.username,
+      isAdmin: user.isAdmin,
+      accessToken: newAccessToken,
+    });
+  });
+})
+
 app.post("/api/login", (req, res) => {
   const { username, password } = req.body;
 
@@ -49,12 +72,10 @@ app.post("/api/login", (req, res) => {
     const refreshToken = generateRefreshToken(user);
 
     refreshTokens.push(refreshToken);
-
-    res.json({
+    res.cookie("refreshToken", refreshToken, {httpOnly: true}).json({
       username: user.username,
       isAdmin: user.isAdmin,
-      accessToken,
-      refreshToken,
+      accessToken
     });
   } else {
     res.status(400).json("Username or password incorrect");
@@ -65,9 +86,9 @@ app.post("/api/login", (req, res) => {
  * once a server receives a refreshtoken and confirms its validation,
  * we return brand new refresh token and access token
  */
-app.post("/api/refresh", (req, res) => {
+app.post("/api/access", (req, res) => {
   // take the refresh token from the user
-  const refreshToken = req.body.token;
+  const refreshToken = req.cookies.refreshToken;
   // send error if there is no token or it is invalid
   if (!refreshToken) return res.status(401).json("You are not authenticated");
   if (!refreshTokens.includes(refreshToken))
@@ -83,9 +104,8 @@ app.post("/api/refresh", (req, res) => {
 
     refreshTokens.push(newRefreshToken);
 
-    res.status(200).json({
+    res.status(200).cookie("refreshToken", newRefreshToken, {httpOnly: true}).json({
       accessToken: newAccessToken,
-      refreshToken: newRefreshToken,
     });
   });
 });
@@ -121,7 +141,7 @@ app.delete("/api/users/:userId", verify, (req, res) => {
 // Taking refresh token out of refreshtokens disables others to get a new access token.
 // Both refresh token and access token required!
 app.post("/api/logout", verify, (req, res) => {
-  const refreshToken = req.body.token;
+  const refreshToken = req.cookies.refreshToken;
   refreshTokens = refreshTokens.filter((token) => token != refreshToken);
   res.status(200).json("You logged out successfully");
 });
